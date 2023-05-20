e927429232991541a5ea0f3e6a4a3e78750471ac
tinmar <martin.hermant@gmail.com>
2023-05-20 15:07:53 +0200

    WIP

--- parseXml.ts
+++ parseXml.ts
@@ -2,6 +2,7 @@ import { kMaxLength } from 'buffer';
 import { XMLParser } from 'fast-xml-parser';
 import { readFileSync, writeFileSync } from 'fs';
 import { resolve, basename, dirname } from 'path';
+import { exit } from 'process';
 
 
 //xml file from https://learn.microsoft.com/en-us/previous-versions/windows/desktop/ms762271(v=vs.85)
@@ -54,17 +55,26 @@ export class XFile {
         return "";
     }
 
-    findId(id: string, allowedTypes: string[]) {
+    findId(id: string, allowedTypes: string[]): any {
         for (let k of allowedTypes) {
-            const res = this.json[k].find((e: any) => e["@_id"] == id)
-            if (res !== undefined) return [res, k];
+            let arr = this.json[k]
+            // if (!Array.isArray(arr))
+            //     arr = [arr];
+            // console.log(k, arr)
+            const res = arr.find((e: any) => e["@_id"] == id)
+            if (res !== undefined) {
+                if (res["@_name"] == undefined) { return this.findId(res["@_type"], allowedTypes) }
+                return [res, k];
+            }
         }
         return [undefined, undefined]
     }
 
 
-    findTypeFromID(id: string) {
-        const TypeNodes = ["Typedef", "FundamentalType", "Class", "Struct"]
+    findTypeFromID(id: string, allowPtr = false) {
+        const TypeNodes = ["Typedef", "FundamentalType", "Class", "Struct", "ElaboratedType", "AtomicType"].concat(allowPtr ? ["ReferenceType"
+            , "PointerType"] : [])
+
         return this.findId(id, TypeNodes)
     }
 
@@ -72,6 +82,8 @@ export class XFile {
     getFieldMetaData(f: any) {
         const tid = f["@_type"]
         const [to, foundType] = this.findTypeFromID(tid)
+        if (to == undefined)
+            throw Error("type of member or arg can not be resolved from xml, name is : " + f["@_name"] + " :: " + tid + " :::::" + JSON.stringify(f))
         let type = to["@_name"];
         let typeContext = this.getContextName(to);
         if (typeContext) type = typeContext + "::" + type
@@ -79,6 +91,8 @@ export class XFile {
         // for(const stdT of ["vector","map","string"])
         // if(type.startsWith(stdT))
         //     type = type.replace(stdT,"std::"+stdT)
+        if (type == undefined)
+            throw Error("type does not have name or ctx : " + JSON.stringify(to))
         const cppType = type;
         const meta = {
             name: f["@_name"],
@@ -94,26 +108,40 @@ export class XFile {
     getMethodMetadata(f: any) {
         const rtid = f["@_returns"]
         const [to, foundType] = this.findTypeFromID(rtid)
+        if (to == undefined)
+            throw Error("type of return value can not be resolved from xml (" + f["@_name"] + ")")
         let returnType = to["@_name"];
         let typeContext = this.getContextName(to);
         if (typeContext) returnType = typeContext + "::" + returnType
 
-        let args = []
-        for (const [k, v] of Object.entries(f)) {
-            if (!k.startsWith("@"))
-                args.push(v)
-        }
+        let args = f["Argument"]
+        if (args == undefined)
+            args = []
+        else if (!Array.isArray(args))
+            args = [args];
 
-        const aF = args.map(e => this.getFieldMetaData(e))
+        const aF = args.map((e: any) => this.getFieldMetaData(e))
         const meta = {
             name: f["@_name"],
             returnType,
-            args: aF
+            args: aF,
+            opts: undefined as (undefined | string[])
             // entry: f,
 
         }
+        const opts = this.getCommentsAtLine(f["@_file"], parseInt(f["@_line"]) - 1)
+        if (opts && opts.length)
+            meta.opts = opts;
         // console.log(foundType, to)
         return meta;
+    }
+
+    getCommentsAtLine(fileId: string, lineN: number) {
+        const [fileObj, t] = this.findId(fileId, ["File"]);
+        const fileP = fileObj["@_name"]
+        const fileL = readFileSync(fileP).toString().split('\n')[lineN]
+        const comments = fileL.split("//")[1]
+        return comments.split(",").map(e => e.trim())
     }
 
     parseFieldOfStruct(structID: string) {
@@ -142,9 +170,15 @@ export class XFile {
         this.fileId = this.getFileId(originalFileName);
 
         let filtered: any = {}
+        // console.log(this.json, Object.keys(this.json), "<<<<")
+        // return
         for (let [k, v] of Object.entries(this.json)) {
             if (k.startsWith("@"))
                 continue
+            if (k.startsWith("AtomicType"))
+                continue
+
+            // console.log(">>>>>>", Object.keys(this.json), k, this.json[k])
             filtered[k] = this.json[k].filter((e: any) => e["@_file"]?.includes(this.fileId));
             if (filtered[k].length == 0)
                 delete filtered[k]
--- test/simple/RootAPI.h
+++ test/simple/RootAPI.h
@@ -14,12 +14,12 @@ struct RootAPI {
   unsigned char counter = 0;
   int intValue = 0;
   std::array<float, 3> color;
-
+  // int *intPtr;
   // void simpleAction() { std::cout << "simple action " << std::endl; }
   void save(){};
   int simpleArg(int arg) { return arg; };
 
-  SimpleMemberClass simpleMemberClass;
+  // SimpleMemberClass simpleMemberClass;
   std::array<long long, 512> universe;
   std::array<SimpleMemberClass, 512> lluniverse;
   std::vector<int> intVector;
